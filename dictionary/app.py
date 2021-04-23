7638eb09309af5124d37a6575f274da9866ef565
trungne <nguyenquochoangtrung@gmail.com>
2021-04-23 20:39:08 +0700

    change post method to get

--- dictionary/app.py
+++ dictionary/app.py
@@ -7,21 +7,21 @@ app = Flask(__name__)
 def main_page():
     # TODO: create a welcome page
     if request.method == "GET":
-        return render_template('main_page.html')
+        return render_template('dictionary.html')
     else: # Post method
-        word = request.form['word']
-        return redirect(url_for('dictionary', word=word))
+        return redirect(url_for('dictionary'))
 
-@app.route('/<word>', methods=["POST", "GET"])
-def dictionary(word):
-    r = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en_US/{word}")
+@app.route('/dictionary')
+def dictionary():
+    r = requests.get(f"https://api.dictionaryapi.dev/api/v2/entries/en_US/{request.args.get('word')}")
     if r.status_code != 200:
         return "Error"
-    else:
-        word = r.json()[0]['word'] 
-        phonetics = r.json()[0]['phonetics']
-        meanings = r.json()[0]['meanings']
-        return render_template('dictionary.html', word=word, phonetics=phonetics, meanings=meanings)
+
+
+    word = r.json()[0]['word'] 
+    phonetics = r.json()[0]['phonetics']
+    meanings = r.json()[0]['meanings']
+    return render_template('dictionary.html', word=word, phonetics=phonetics, meanings=meanings)
 
 def main():
     app.run(debug=True)
